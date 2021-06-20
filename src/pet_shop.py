9338e51073bd31bdaf56a3229e17f49d7dc6ab91
Sara <sluisdejesus@gmail.com>
2021-06-20 21:17:01 +0100

    16 done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -46,3 +46,6 @@ def get_customer_cash(customer):
     
 def remove_customer_cash(customer,cash):
     customer ["cash"] -= cash
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
