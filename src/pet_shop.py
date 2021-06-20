c52f065e0c9e0b43fd568e090733c5d1e9733afd
Sara <sluisdejesus@gmail.com>
2021-06-20 21:19:24 +0100

    16 done, MPV finished

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -49,3 +49,6 @@ def remove_customer_cash(customer,cash):
 
 def get_customer_pet_count(customer):
     return len(customer["pets"])
+
+def add_pet_to_customer(customer,new_pet):
+    customer["pets"].append(new_pet)
