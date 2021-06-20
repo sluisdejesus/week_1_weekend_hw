bdeda5577bef66a6ac2d33911cc330791d9a0a07
Sara <sluisdejesus@gmail.com>
2021-06-20 21:04:10 +0100

    14 done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -40,3 +40,6 @@ def remove_pet_by_name(pet_shop, name):
 
 def add_pet_to_stock (add_stock, pet):
     add_stock["pets"].append(pet)
+
+def get_customer_cash(customer):
+    return customer["cash"]
